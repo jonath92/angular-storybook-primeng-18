77c85ca258f1f2e9b8d7d6c4c029cfe6dfbb798e
Jonathan Sarrazin <jonathan.sarrazin@prodyna.com>
2024-11-20 20:00:33 +0100

    add tailwind to angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from "./button.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [ButtonComponent],
+  template: `
+  <app-button/>
+  `,
 })
 export class AppComponent {
   title = 'angular-vite-tailwind-storybook';
---
+++ src/app/button.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  template: `<div
+    class="bg-gray-500 text-white text-3xl font-bold px-5 py-3 rounded-md"
+  >
+    Button
+  </div>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonComponent {}
