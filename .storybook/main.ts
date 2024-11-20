08e73032f76ac85bf9e42e21a0f05690d5dfa890
Jonathan Sarrazin <jonathan.sarrazin@prodyna.com>
2024-11-20 20:10:35 +0100

    update to analog storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,45 @@
-import type { StorybookConfig } from "@storybook/angular";
+import { StorybookConfig } from '@storybook/angular';
+import { StorybookConfigVite } from '@storybook/builder-vite';
+import { UserConfig } from 'vite';
 
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+const config: StorybookConfig & StorybookConfigVite = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/angular",
+    name: '@storybook/angular',
     options: {},
+  },
+  core: {
+    builder: {
+      name: '@storybook/builder-vite',
+      options: {
+        viteConfigPath: undefined,
+      },
+    },
+  },
+  async viteFinal(config: UserConfig) {
+    // Merge custom configuration into the default config
+    const { mergeConfig } = await import('vite');
+    const { default: angular } = await import('@analogjs/vite-plugin-angular');
+
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: [
+          '@storybook/angular',
+          '@storybook/angular/dist/client',
+          '@angular/compiler',
+          '@storybook/blocks',
+          'tslib',
+        ],
+      },
+      plugins: [angular({ jit: true, tsconfig: './.storybook/tsconfig.json' })],
+    });
   },
 };
 export default config;
--- src/app/button.component.ts
+++ src/app/button.component.ts
@@ -4,11 +4,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-button',
   standalone: true,
   imports: [],
-  template: `<div
-    class="bg-gray-500 text-white text-3xl font-bold px-5 py-3 rounded-md"
-  >
-    Button
-  </div>`,
+  template: ` <div>Test</div>
+    <div class="bg-gray-500 text-white text-3xl font-bold px-5 py-3 rounded-md">
+      Button
+    </div>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonComponent {}
