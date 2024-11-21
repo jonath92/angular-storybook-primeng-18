da7537770141253da7a46cb9b7bd613257908e74
Jonathan Sarrazin <jonathan.sarrazin@prodyna.com>
2024-11-21 17:55:04 +0100

    add primengv17

--- src/app/button.component.ts
+++ src/app/button.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
-  template: ` <div class="test">Test</div>
+  imports: [ButtonModule],
+  template: ` <p-button label="Check" icon="pi pi-check"></p-button>
+
+    <div class="test">Test</div>
     <div class="bg-gray-500 text-white text-3xl font-bold px-5 py-3 rounded-md">
       Button
     </div>`,
